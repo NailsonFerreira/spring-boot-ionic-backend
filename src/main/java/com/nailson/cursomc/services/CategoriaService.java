ef083f60f3c5c129ce8dbc31b4a58d1909182096
Nailson <nailson_ferreira@hotmail.com>
2019-12-01 11:54:44 -0300

    aula 33 delete categoria

--- src/main/java/com/nailson/cursomc/services/CategoriaService.java
+++ src/main/java/com/nailson/cursomc/services/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nailson.cursomc.domain.Categoria;
 import com.nailson.cursomc.repositories.CategoriaRepository;
+import com.nailson.cursomc.services.exceptions.DataIntegrityException;
 import com.nailson.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -35,5 +37,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		try {
+			find(id);
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
