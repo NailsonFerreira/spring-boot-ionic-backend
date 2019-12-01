da975c88b962846a177dc6d1b2e91bd07b935a9f
Nailson <nailson_ferreira@hotmail.com>
2019-12-01 16:43:29 -0300

    aula 35

--- src/main/java/com/nailson/cursomc/CursomcApplication.java
+++ src/main/java/com/nailson/cursomc/CursomcApplication.java
@@ -72,6 +72,13 @@ public class CursomcApplication implements CommandLineRunner{
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletronicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Livraria");
+		Categoria cat9 = new Categoria(null, "Escritório");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 300.0);
@@ -84,7 +91,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepos.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepos.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9));
 		produtoRepos.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Pernambuco");
